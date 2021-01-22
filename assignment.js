cbfc8106847f76008ad3bac9238e393020455c12
Mizanur Rahman <mizzanur007@gmail.com>
2021-01-22 20:15:51 +0600

    ✔ Finished the first problem solving

--- assignment.js
+++ assignment.js
@@ -1,8 +1,30 @@
 /*-----------------------------------------------------------------
 -------------------------------------------------------------------
-------------- Author Name : Mizanur Rahman-------------------------
+------------- Author Name : Mizanur Rahman ------------------------
 ------------- Assignmnet 3 on JavaScript Problem Solving ----------
 -------------------------------------------------------------------*/
 
 // Here is the Github repo link of the assignment
 // https://github.com/Mizzan/javaScript-assignment
+
+/*-----------------------------------------------------------------
+-------------------------------------------------------------------
+------------------------Problem Solving #1 ------------------------
+------------------------ kilometerToMeter -------------------------
+-------------------------------------------------------------------*/
+function kilometerToMeter(kilometerValue) {
+  if (kilometerValue <= 0) {
+    // checking if it is positive number
+    return "Kilometer value should be positive, Enter positive number.";
+  } else if (isNaN(kilometerValue)) {
+    // checking if it is a string
+    return "Kilometer value should not be string, Enter valid number.";
+  } else {
+    // multipying 1000 meter to kilometerValue as [ 1 km = 1000 meter]
+    return kilometerValue * 1000;
+  }
+}
+
+// kilometerToMeter("stringCheck");
+// kilometerToMeter(-23);
+// kilometerToMeter(23);
