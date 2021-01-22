070397b2dda46a99f1aff5e89e4dbb6d8b7aa0ac
Mizanur Rahman <mizzanur007@gmail.com>
2021-01-22 18:16:30 +0600

    ✔ Added the base assignment js file to the repo

---
+++ assignment.js
@@ -0,0 +1,8 @@
+/*-----------------------------------------------------------------
+-------------------------------------------------------------------
+------------- Author Name : Mizanur Rahman-------------------------
+------------- Assignmnet 3 on JavaScript Problem Solving ----------
+-------------------------------------------------------------------*/
+
+// Here is the Github repo link of the assignment
+// https://github.com/Mizzan/javaScript-assignment
