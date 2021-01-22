cf9e9897e657326abe7f7bf6873564f1b043c751
Mizanur Rahman <mizzanur007@gmail.com>
2021-01-22 20:25:14 +0600

    ✔ Finished the second problem solving

--- assignment.js
+++ assignment.js
@@ -12,6 +12,7 @@
 ------------------------Problem Solving #1 ------------------------
 ------------------------ kilometerToMeter -------------------------
 -------------------------------------------------------------------*/
+
 function kilometerToMeter(kilometerValue) {
   if (kilometerValue <= 0) {
     // checking if it is positive number
@@ -25,6 +26,40 @@ function kilometerToMeter(kilometerValue) {
   }
 }
 
-// kilometerToMeter("stringCheck");
-// kilometerToMeter(-23);
-// kilometerToMeter(23);
+// kilometerToMeter("stringCheck"); // string check
+// kilometerToMeter(-23); // negative number check
+// kilometerToMeter(23);  // expected result check
+
+/*-----------------------------------------------------------------
+-------------------------------------------------------------------
+------------------------Problem Solving #2 ------------------------
+------------------------ budgetCalculator -------------------------
+-------------------------------------------------------------------*/
+
+function budgetCalculator(numberOfWatch, numberOfPhone, numberOfLaptop) {
+  var watchPrice = 50;
+  var phonePrice = 100;
+  var laptopPrice = 500;
+
+  if (isNaN(numberOfWatch) || numberOfWatch <= 0) {
+    //   checking if it is a string or a negative number
+    return "Please enter a valid number of numberOfWatch.";
+  } else if (isNaN(numberOfPhone) || numberOfPhone <= 0) {
+    //   checking if it is a string or a negative number
+    return "Please enter a valid number of numberOfPhone.";
+  } else if (isNaN(numberOfLaptop) || numberOfLaptop <= 0) {
+    //   checking if it is a string or a negative number
+    return "Please enter a valid number of numberOfLaptop.";
+  } else {
+    return (
+      //   calculating the total budget
+      numberOfWatch * watchPrice +
+      numberOfPhone * phonePrice +
+      numberOfLaptop * laptopPrice
+    );
+  }
+}
+
+// budgetCalculator(5, "asd", 2); // string check
+// budgetCalculator(-3, 5, 2); // negative number check
+// budgetCalculator(5, 3, 2); // expected number check
